89bcfd9fa2f813805dd51f8bf826cbbae007d564
shivam <shiv.kurtarkar@gmail.com>
2022-11-07 23:05:24 +0530

    indexer updated preprocessing

--- vecsim/indexer.py
+++ vecsim/indexer.py
@@ -144,8 +144,40 @@ class RedisIndexer():
 #     df.reset_index(drop=True, inplace=True)
 #     return df
 
+def clean_description(description: str):
+    if not description:
+        return ""
+    # remove unicode characters
+    description = description.encode('ascii', 'ignore').decode()
+
+    # remove punctuation
+    description = re.sub('[%s]' % re.escape(string.punctuation), ' ', description)
+
+    # clean up the spacing
+    description = re.sub('\s{2,}', " ", description)
+
+    # remove urls
+    #description = re.sub("https*\S+", " ", description)
+
+    # remove newlines
+    description = description.replace("\n", " ")
+
+    # remove all numbers
+    #description = re.sub('\w*\d+\w*', '', description)
+
+    # split on capitalized words
+    description = " ".join(re.split('(?=[A-Z])', description))
+
+    # clean up the spacing again
+    description = re.sub('\s{2,}', " ", description)
+
+    # make all words lowercase
+    description = description.lower()
+
+    return description
+
 def preprocess(r):
-    return r.title + r.abstract
+    return clean_description(r.title + r.abstract)
 
 async def main(filename):
     docs_source = DocumentSource(filename)
