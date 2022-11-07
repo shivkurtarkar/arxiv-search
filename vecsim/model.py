75b752672c79ff128d1453573e23f38e4e2644f0
shivam <shiv.kurtarkar@gmail.com>
2022-11-07 22:29:58 +0530

    updated model files to use cuda

--- vecsim/model.py
+++ vecsim/model.py
@@ -1,77 +1,10 @@
-from transformers import AutoTokenizer,AutoModel, PreTrainedModel,PretrainedConfig
+from transformers import AutoTokenizer
 from typing import Dict
 import torch
 import numpy as np
 
-class ColBERTConfig(PretrainedConfig):
-    model_type = "ColBERT"
-    bert_model: str
-    compression_dim: int = 768
-    dropout: float = 0.0
-    return_vecs: bool = False
-    trainable: bool = True
+from colbert_model import ColBERT
 
-class ColBERT(PreTrainedModel):
-    """
-    ColBERT model from: https://arxiv.org/pdf/2004.12832.pdf
-    We use a dot-product instead of cosine per term (slightly better)
-    """
-    config_class = ColBERTConfig
-    base_model_prefix = "bert_model"
-
-    def __init__(self,
-                 cfg) -> None:
-        super().__init__(cfg)
-        
-        self.bert_model = AutoModel.from_pretrained(cfg.bert_model)
-
-        for p in self.bert_model.parameters():
-            p.requires_grad = cfg.trainable
-
-        self.compressor = torch.nn.Linear(self.bert_model.config.hidden_size, cfg.compression_dim)
-
-    def forward(self,
-                query: Dict[str, torch.LongTensor],
-                document: Dict[str, torch.LongTensor]):
-
-        query_vecs = self.forward_representation(query)
-        document_vecs = self.forward_representation(document)
-
-        score = self.forward_aggregation(query_vecs,document_vecs,query["attention_mask"],document["attention_mask"])
-        return score
-
-    def forward_representation(self,
-                               tokens,
-                               sequence_type=None) -> torch.Tensor:
-        
-        vecs = self.bert_model(**tokens)[0] # assuming a distilbert model here
-        vecs = self.compressor(vecs)
-
-        # if encoding only, zero-out the mask values so we can compress storage
-        if sequence_type == "doc_encode" or sequence_type == "query_encode": 
-            vecs = vecs * tokens["tokens"]["mask"].unsqueeze(-1)
-
-        return vecs
-
-    def forward_aggregation(self,query_vecs, document_vecs,query_mask,document_mask):
-        
-        # create initial term-x-term scores (dot-product)
-        score = torch.bmm(query_vecs, document_vecs.transpose(2,1))
-
-        # mask out padding on the doc dimension (mask by -1000, because max should not select those, setting it to 0 might select them)
-        exp_mask = document_mask.bool().unsqueeze(1).expand(-1,score.shape[1],-1)
-        score[~exp_mask] = - 10000
-
-        # max pooling over document dimension
-        score = score.max(-1).values
-
-        # mask out paddding query values
-        score[~(query_mask.bool())] = 0
-
-        # sum over query values
-        score = score.sum(-1)
-
-        return score
 
 #
 # init the model & tokenizer (using the distilbert tokenizer)
@@ -88,8 +21,10 @@ def get_model():
 
 class ColBERTModel:
     def __init__(self):
+        self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        print("using device:", self.device)
         self.tokenizer = get_tokenizer()
-        self.model = get_model()
+        self.model = get_model().to(self.device)        
         
         # self.get_key=key_format_fn
         # key = self.get_key(i)
@@ -99,7 +34,7 @@ class ColBERTModel:
         #     }
     
     def _preprocess_query(self, data):
-        query_input = self.tokenizer(data)
+        query_input = self.tokenizer(data, return_tensors="pt").to(self.device)
         query_input.input_ids += [103] * 8 # [MASK]
         query_input.attention_mask += [1] * 8
         query_input["input_ids"] = torch.LongTensor(query_input.input_ids).unsqueeze(0)
@@ -107,7 +42,7 @@ class ColBERTModel:
         return query_input
 
     def _preprocess_document(self, data):
-        doc_input = self.tokenizer(data, return_tensors="pt")
+        doc_input = self.tokenizer(data, return_tensors="pt").to(self.device)
         return doc_input
     
     def _compute_representation(self, emb):
