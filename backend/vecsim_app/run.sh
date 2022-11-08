8fabd8dddf0740f6e3d2e0889881105a3fbd3fdf
shivam <shiv.kurtarkar@gmail.com>
2022-11-08 08:50:11 +0530

    updated run.sh script

--- backend/vecsim_app/run.sh
+++ backend/vecsim_app/run.sh
@@ -1,9 +1,14 @@
-#!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo changing dir to ${SCRIPT_DIR}
 cd ${SCRIPT_DIR}
-echo $(pwd)
+echo pwd
 
+
+# uvicorn main:app --reload
+
+#!/bin/sh
+
+# python load_data.py
 cd ../
 pip install -e .
 cd vecsim_app
